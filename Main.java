663de2e5741072876ea60aae83bbb1c3d043516d
Muhannad Khaled <muhannad.khaled@stud.hs-hannover.de>
2026-05-12 10:27:50 +0200

    .gitignore ist erstellt

--- Main.java
+++ Main.java
@@ -5,6 +5,20 @@ public class Main
 {
     public static void main(String args[])
     {
+        int summe =0;
+        for(int i = 1;i<=20;i++)
+        {
+            summe +=i;
+            
+        }
+        int mult= 1;
+        for(int i =1;i<=10;i++)
+        {
+            mult*= i;
+        }
+        System.out.println("Summe: " +summe);
+        System.out.println("Mult= " +mult);
+        
         
     }
 }
