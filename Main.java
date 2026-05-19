a834309dbb254538dbdc895fffe4ee347d8c8e7f
Muhannadkd <mudi37782@gmail.com>
2026-05-19 09:20:54 +0200

    Update 1

--- Main.java
+++ Main.java
@@ -22,8 +22,11 @@ public class Main
         System.out.println("Summe wurde berechnet");
 
         System.out.println("Multiplikation wurde berechnet");
-
+        //Master Update1 
+        //Master update2
+        
         
         
     }
+    
 }
