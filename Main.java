dffbaaaa6613550c9f325b18e906087b83881e8c
Muhannadkd <mudi37782@gmail.com>
2026-05-19 08:46:45 +0200

    Update Main.java
    Änderung

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ public class Main
 {
     public static void main(String args[])
     {
-        int summe =20;
+        double summe =20;
         for(int i = 1;i<=20;i++)
         {
             summe +=i;
