e6101b8d172e8e9a4158cb0ccdfaf624c1efa720
Muhannadkd <mudi37782@gmail.com>
2026-05-19 09:21:41 +0200

    update 3

--- Main.java
+++ Main.java
@@ -24,6 +24,7 @@ public class Main
         System.out.println("Multiplikation wurde berechnet");
         //Master Update1 
         //Master update2
+        //Master update3
         
         
         
